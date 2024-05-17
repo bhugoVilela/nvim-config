87cb2b5dfdfaeee83f527612b052d0eaea2a7a3a
Hugo Vilela <41992011+bhugoVilela@users.noreply.github.com>
2024-05-17 13:53:08 +0100

    fixed config for linux

--- init.lua
+++ init.lua
@@ -6,6 +6,22 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+Lib = {
+  IsLinux = function()
+    return vim.loop.os_uname().sysname == 'Linux'
+  end,
+
+  IsMac = function()
+    return vim.loop.os_uname().sysname == 'Darwin'
+  end,
+
+  WhenOs = function(table)
+    local entry = table[vim.loop.os_uname().sysname]
+    if not entry then return end
+    return entry()
+  end
+}
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
